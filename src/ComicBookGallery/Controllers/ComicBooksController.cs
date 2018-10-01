e3a517c43db1dbd2485adae0ddba00efba0cdebb
DESKTOP-LFO6JU9\Dion <dion.clarke@education.gov.uk>
2018-10-01 15:29:52 +0100

    Updated Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,19 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()  //Ability to view the details from comic book gallery. Adding Action to controller.
         {
+            ViewBag.SeriesTitle = "The Amazing Spiderman";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+
+            var artists = new string[]
+            {
+                 "Script: Dan Slott",
+                 "Pencils: Humberto Ramos",
+                 "Inks: Victor Olazaba",
+                 "Colors: Edgar Delgado",
+                 "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
 
