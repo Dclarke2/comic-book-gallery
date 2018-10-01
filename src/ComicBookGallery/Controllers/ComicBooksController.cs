af8f203fd7543892cd36f8b9677df13565130a57
DESKTOP-LFO6JU9\Dion <dion.clarke@education.gov.uk>
2018-10-01 11:47:23 +0100

    Updated detail action method to use action result types

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,11 +6,19 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller
+    public class ComicBooksController : Controller      //<-- Must be called Controller & Inherit from controller base class. ComicBooksController to handle request...
     {
-        public string Detail()
+        public ActionResult Detail()  //Ability to view the details from comic book gallery. Adding Action to controller.
         {
-            return "Hello from the Comic Books controller!";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+
+                return Redirect("/");
+
+            }
+            return Content("Hello from the Comic Books controller!");
+            
+            
 
         }
 
