bf33817d6ba0d1532baa468a146702a3832c925e
DESKTOP-LFO6JU9\Dion <dion.clarke@education.gov.uk>
2018-10-02 12:37:41 +0100

    Updated Comic Book detail view to be strongly typed

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -7,26 +7,27 @@ namespace ComicBookGallery.Models
 {
     public class ComicBook
     {
-        public int Id { get; set; }
-        public string SeriesTitle { get; set; }
-        public int IssueNumber { get; set; }
-        public string DescriptionHtml { get; set; }
-        public Artist[] Artists { get; set; }
-        public bool Favorite { get; set; }
+        public int Id { get; set; } //Unique ID to identify each comic.
+        public string SeriesTitle { get; set; } //Represents title of series comic book is apart of. 
+        public int IssueNumber { get; set; } //IssueNumber release
+        public string DescriptionHtml { get; set; } //Description of comic book.
+        public Artist[] Artists { get; set; } //Artists involved in created of comic.
+        public bool Favorite { get; set; } //Indicates whether comic is a favorite or not.
 
         public string DisplayText
         {
-            get
+            get         //String value to uniquely identify for our comic book users. Property will be ReadOnly.Accompolished by not setting a 'Setter'. 
             {
                 return SeriesTitle + " #" + IssueNumber;
             }
         }
 
-        //series-title-issuenumber.jpg
-        public string CoverImageFileName
+        //series-title-issuenumber.jpg <-- Shows structure 
+        public string CoverImageFileName //Displays File name of comic book. 
         {
             get
             {
+                //'Replace' replaces any spaces in series title with Hypens. First parameter is string to replace followed by what to replace with. 
                 return SeriesTitle.Replace("", "-").ToLower() + "-" + IssueNumber + ".jpg";
             }
             
